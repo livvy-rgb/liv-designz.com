8f853a0d952cc90f4b3cac3ccb171e87f43e8b5e
livvy-rgb <112041722+livvy-rgb@users.noreply.github.com>
2023-04-10 19:37:46 +1200

    update 1

--- js/script.js
+++ js/script.js
@@ -22,64 +22,64 @@ burger.addEventListener("click", () => {
 
 //var textWrapper = document.querySelector(".loader-text");
 //textWrapper.innerHTML = textWrapper.textContent.replace(
-    ///\S/g,
+  //  /\S/g,
     //"<span class='letter'>$&</span"
 //);
 
 //anime.timeline().add({
-    //targets: ".loader-text .letter",
-    //translateX: [40, 0],
-    //translateZ: 0,
-    //opactiy: [0, 1],
-    //easing: "easeOutExpo",
-    //duration: 1200,
-   // delay: (el, i) => 2400 + 30 * i,
+  //  targets: ".loader-text .letter",
+  //  translateX: [40, 0],
+  //  translateZ: 0,
+  //  opactiy: [0, 1],
+   // easing: "easeOutExpo",
+  //  duration: 1200,
+  //  delay: (el, i) => 2400 + 30 * i,
 //});
 
 //gsap.from(".loader-wrapper", 2, {
-    //scale: 0.8,
-    //ease: "power1.inOut",
+//    scale: 0.8,
+ //   ease: "power1.inOut",
 //});
 
 //gsap.from(".loader", 2, {
-    //top: "100%",
-    //ease: "power3.inOut",
+ //   top: "100%",
+ //   ease: "power3.inOut",
 //});
 
 //gsap.to(".loader-wrapper, .pre-loader", 1, {
-    //scale: 1.2,
-    //opactiy: 0,
-   // display: "none",
-    //ease: "power3.inOut",
-    //delay: 2,
+ //   scale: 1.2,
+  //  opactiy: 0,
+  //   display: "none",
+ //   ease: "power3.inOut",
+ //   delay: 2,
 //},
 //"-=1"
 //);
 
 //gsap.from(".revealer", 1, {
-    //width: "0",
-    //ease: "power3.inOut",
-    //delay: 3,
+ //   width: "0",
+  //  ease: "power3.inOut",
+  //  delay: 3,
 //});
 
 //gsap.to(".revealer", 0.8, {
-    //left: "100%",
-    //ease: "power3.inOut",
-    //delay: 4,
+ //   left: "100%",
+  //  ease: "power3.inOut",
+  //  delay: 4,
 //});
 
 //gsap.to(".loader-text-wrapper", 0.3, {
-    //opactiy: 0,
-    //display: "none",
-    //ease: "power3.inOut",
-    //delay: 4,
+ //   opactiy: 0,
+ //   display: "none",
+  //  ease: "power3.inOut",
+  //  delay: 4,
 //});
 
 //gsap.to(".loader-text-wrapper", 0.3, {
-    //opactiy: 0,
-    //display: "none",
-    //ease: "power3.inOut",
-    //delay: 4,
+  //  opactiy: 0,
+  //  display: "none",
+   // ease: "power3.inOut",
+  //  delay: 4,
 //});
 
 
